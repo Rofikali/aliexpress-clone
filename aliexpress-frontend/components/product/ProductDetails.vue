452b12c9eb018f5d1e314c92bf1502b357e6adc9
Rofik Don <alirofikr@gmail.com>
2025-09-15 08:18:24 +0530

    have so many things to da

--- aliexpress-frontend/components/product/ProductDetails.vue
+++ aliexpress-frontend/components/product/ProductDetails.vue
@@ -2,16 +2,6 @@
     <div>
         <div v-if="product" class="md:flex gap-4 justify-between mx-auto w-full">
             <div class="md:w-[40%]">
-                <!-- Main Product Image -->
-                <!-- <NuxtImg v-if="currentImage" class="rounded-lg object-contain" :src="currentImage"
-                    :alt="product.value?.title || 'Product Image'" loading="lazy" />
-                <div v-if="images.length" class="flex items-center justify-center mt-2 gap-2">
-                    <NuxtImg v-for="(image, idx) in images" :key="idx" @mouseover="currentImage = image"
-                        @click="currentImage = image" width="70"
-                        class="rounded-md object-cover border-[3px] cursor-pointer transition"
-                        :class="currentImage === image ? 'border-[#FF5353]' : 'border-transparent'" :src="image"
-                        :alt="`Thumbnail ${idx + 1}`" loading="lazy" />
-                </div> -->
                 <ProductCard :product="product" />
             </div>
             <!-- Product Details -->
@@ -70,7 +60,8 @@
 
 <script setup>
 import { ref, watchEffect, computed } from 'vue'
-import { useUserStore } from '~/stores/user'
+// import { useUserStore } from '~/stores/user'
+import { useAuthStore } from '~/stores/modules/authStore'
 
 const props = defineProps({
     product: {
@@ -82,11 +73,11 @@ const props = defineProps({
 
 const { product } = toRefs(props)
 
-const userStore = useUserStore()
+const userStore = useAuthStore()
 
-const isInCart = computed(() =>
-    userStore.cart.some(prod => prod.id === product.value?.id)
-)
+// const isInCart = computed(() =>
+//     userStore.cart.some(prod => prod.id === product.value?.id)
+// )
 
 const priceComputed = computed(() => {
     return product.value?.price ? (product.value.price / 100).toFixed(2) : '0.00'
