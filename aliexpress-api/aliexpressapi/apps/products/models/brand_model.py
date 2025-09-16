681c119eb614a18a6d97259d62c9183d6ea70d00
simi <simiprinters049@gmail.com>
2025-09-16 17:02:55 +0530

    added nested route for products attributes

---
+++ aliexpress-api/aliexpressapi/apps/products/models/brand_model.py
@@ -0,0 +1,20 @@
+from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    logo = models.URLField(max_length=500, blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "products_brand"
+
+    def __str__(self):
+        return self.name
