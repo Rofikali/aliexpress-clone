aee14d3f726d8b41a94b2070b70cc1469bada284
simi <simiprinters049@gmail.com>
2025-10-08 16:51:31 +0530

    updating categorie frontend

--- aliexpress-api/aliexpressapi/apps/home/serializers/banner_serializer.py
+++ aliexpress-api/aliexpressapi/apps/home/serializers/banner_serializer.py
@@ -12,10 +12,33 @@ class HomepageBannerSerializer(serializers.ModelSerializer):
             "id",
             "title",
             "image",
-            # "link_url",
             "alt_text",
             "sort_order",
             "is_active",
+        ]
+
+    def get_image(self, obj):
+        request = self.context.get("request")
+        if obj.image and request:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url if obj.image else None
+
+
+class HomepageBannerDetailSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = HomepageBanner
+        fields = [
+            "id",
+            "title",
+            "image",
+            "alt_text",
+            "alt_text",
+            "sort_order",
+            "is_active",
+            "created_at",
+            "updated_at",
         ]
 
     def get_image(self, obj):
